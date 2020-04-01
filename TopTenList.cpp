5c71cc253ed9cb1746de83b5e1e71c19a03f6c80
dragongoat <japawek@gmail.com>
2020-03-31 20:17:28 -0700

    Made changes from Week 10 - Monday video

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -1,21 +1,36 @@
 //Manages a list of top ten hyperlinks
 #include <string>
+#include <iostream>
 #include "TopTenList.h"
 #include "Hyperlink.h"
 
 using std::string;
+using std::cout;
+using std::endl;
 
 TopTenList::TopTenList()
 {
     _list.resize(10);
 }
 
+void TopTenList::display_forward() {
+    for (unsigned int i = 0; i < _list.size(); i++) {
+        cout << i + 1 << ": " << _list.at(i).text << endl;
+    }
+}
+
+void TopTenList::display_backward() {
+    for (int i = _list.size() - 1; i >= 0; i--) {
+        cout << i + 1 << ": " << get(i).text << endl;
+    }
+}
+
 void TopTenList::set_at(int index, Hyperlink link)
 {
-    _list.at(index) = link;
+    _list.at(index - 1) = link;
 }
 
 Hyperlink TopTenList::get(int index)
 {
-    return _list.at(index);
+    return _list.at(index - 1);
 }
